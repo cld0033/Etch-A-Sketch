ccbb5e0b0785837827262a4de56e093376dd83ab
Chester Davidson <cld0033@auburn.edu>
2024-04-30 16:22:29 -0400

    added click event to grid to change color.

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const gridContainer = document.getElementById('container');
 
 function makeGrid(rows, cols) {
-    gridContainer.style.setProperty('--grid-rows', rows);
-    gridContainer.style.setProperty('--grid-cols', cols);
+  gridContainer.style.setProperty('--grid-rows', rows);
+  gridContainer.style.setProperty('--grid-cols', cols);
 
-    for(i = 0; i < (rows * cols); i++) {
-        let cell = document.createElement('div');
-        //cell.innerText = (i + 1);
-        gridContainer.appendChild(cell).className = 'grid-item';
-    }
+  for (i = 0; i < rows * cols; i++) {
+    let cell = document.createElement('div');
+    gridContainer.appendChild(cell).className = 'grid-item';
+  }
 }
-
 makeGrid(16, 16);
+
+const gridElement = document.querySelectorAll('.grid-item');
+gridElement.forEach((gridElement) => {
+  gridElement.addEventListener('click', () => {
+    gridElement.style.backgroundColor = 'white';
+  });
+});
