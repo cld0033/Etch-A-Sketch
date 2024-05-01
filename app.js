7edc7f9fa7d5722e09f6899a1e3590cac2cb4b89
Chester Davidson <cld0033@auburn.edu>
2024-05-01 14:15:54 -0400

    1) added a reset button to reset grid to 16x16 2) changed background color to a charcoal grey

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
-const gridContainer = document.getElementById('container');
+const h1 = document.createElement('h1');
+h1.innerText = 'Etch-A-Sketch';
+h1.setAttribute('id', 'sub-heading');
+h1.setAttribute('class', 'secondary');
+document.body.appendChild(h1);
 
+//contains the grid
+const gridContainer = document.createElement('div');
+gridContainer.innerText = '';
+gridContainer.setAttribute('id', 'container');
+document.body.appendChild(gridContainer);
+
+let rows = 0;
+let cols = 0;
+
+//function to make the grid.
 function makeGrid(rows, cols) {
   gridContainer.style.setProperty('--grid-rows', rows);
   gridContainer.style.setProperty('--grid-cols', cols);
@@ -8,12 +22,32 @@ function makeGrid(rows, cols) {
     let cell = document.createElement('div');
     gridContainer.appendChild(cell).className = 'grid-item';
   }
+
+  const gridElement = document.querySelectorAll('.grid-item');
+  gridElement.forEach((gridElement) => {
+    gridElement.addEventListener('click', () => {
+      gridElement.style.backgroundColor = 'white';
+    });
+  });
 }
+//initially sets grid when page is loaded
 makeGrid(16, 16);
 
-const gridElement = document.querySelectorAll('.grid-item');
-gridElement.forEach((gridElement) => {
-  gridElement.addEventListener('click', () => {
-    gridElement.style.backgroundColor = 'white';
-  });
+//adds a div element to add button options.
+const buttonGroup = document.createElement('div');
+buttonGroup.innerText = '';
+buttonGroup.setAttribute('class', 'options');
+document.body.appendChild(buttonGroup);
+
+//creates a reset button to erase and set grid.
+let reset = document.createElement('button');
+reset.setAttribute('id', 'resetButton');
+reset.innerText = 'Reset';
+buttonGroup.appendChild(reset);
+
+//add functionality to the reset button
+const resetGrid = document.getElementById('resetButton');
+resetGrid.addEventListener('click', () => {
+  gridContainer.innerText = '';
+  makeGrid(16, 16);
 });
